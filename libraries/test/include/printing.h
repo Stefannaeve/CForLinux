992ef68984b5cac2c6793dbb86444442451744e1
stefannaeve <stefan.naeve@hotmail.com>
2024-03-03 16:50:36 +0000

    Made a place for all future libraries

---
+++ libraries/test/include/printing.h
@@ -0,0 +1,10 @@
+//
+// Created by stefannaeve on 3/3/24.
+//
+
+#ifndef __PRINTING_H__
+#define __PRINTING_H__
+
+int printing(char *array);
+
+#endif //__PRINTING_H__
---
+++ libraries/test/include/snBubblesort.h
@@ -0,0 +1,10 @@
+//
+// Created by Stefan on 02/03/2024.
+//
+
+#ifndef __SNBUBBLESORT_H__
+#define __SNBUBBLESORT_H__
+
+int bubbleSort(int length, int *array);
+
+#endif //__SNBUBBLESORT_H__
