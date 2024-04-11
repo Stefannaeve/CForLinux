8ad756e67039a836044effab70eedcec1a66382a
stefannaeve <stefan.naeve@hotmail.com>
2024-04-11 19:02:37 +0100

    Small changes

--- Uke10/homeExam2021/src/client.c
+++ Uke10/homeExam2021/src/client.c
@@ -35,96 +35,90 @@ int client() {
 
     if(result != 0){
         printf("Failed getting ip address from web server - Error message: %s\n", strerror(errno));
-        return 1;
     }
+    else {
 
-    struct sockaddr_in* internet_addr = (struct sockaddr_in*) addr->ai_addr;
+		struct sockaddr_in* internet_addr = (struct sockaddr_in*) addr->ai_addr;
 
-    printf("ikke.no is at: %s\n", inet_ntoa(internet_addr->sin_addr));
-    internet_addr->sin_port = htons(iPort);
-    printf("ikke.no is now using port: %d\n", ntohl(internet_addr->sin_port));
-    printf("ikke.no is using family: %u. 2 is AF_INET\n", internet_addr->sin_family);
+		printf("ikke.no is at: %s\n", inet_ntoa(internet_addr->sin_addr));
+		internet_addr->sin_port = htons(iPort);
+		printf("ikke.no is now using port: %d\n", ntohl(internet_addr->sin_port));
+		printf("ikke.no is using family: %u. 2 is AF_INET\n", internet_addr->sin_family);
 
-    sockFd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockFd < 0) {
-        printf("Failed to create endpoint and retrieve file descriptor - Error message %s\n", strerror(errno));
-        return 1;
-    }
-    printf("Successfully created endpoint with socket\n");
+		sockFd = socket(AF_INET, SOCK_STREAM, 0);
+		if (sockFd < 0) {
+		    printf("Failed to create endpoint and retrieve file descriptor - Error message %s\n", strerror(errno));
+		} else {
+            printf("Successfully created endpoint with socket\n");
 
-    saAddr.sin_family = AF_INET;
-    saAddr.sin_port = htons(iPort);
-    saAddr.sin_addr.s_addr = htonl(0x7F000001); //Home
-
-    if (connect(sockFd, (struct sockaddr *) internet_addr, sizeof(*internet_addr)) < 0) {
-        close(sockFd);
-        sockFd = -1;
-        printf("Connection failed - Error message: %s\n", strerror(errno));
-        return 1;
-    }
-    printf("Connect successfully handled\n");
+            if (connect(sockFd, (struct sockaddr *) internet_addr, sizeof(*internet_addr)) < 0) {
+                close(sockFd);
+                sockFd = -1;
+                printf("Connection failed - Error message: %s\n", strerror(errno));
+                return 1;
+            } else {
+                printf("Connect successfully handled\n");
 
 
-    memset(buffer, 0, BUFFERSIZE);
+                memset(buffer, 0, BUFFERSIZE);
 
-    value = send(sockFd, request, strlen(request), 0);
+                value = send(sockFd, request, strlen(request), 0);
 
-    if (value < 0) {
-        printf("Failed to transmit message - Error massage: %s\n", strerror(errno));
-    }
+                if (value < 0) {
+                    printf("Failed to transmit message - Error massage: %s\n", strerror(errno));
+                } else {
 
-    readValue = read(sockFd, buffer, BUFFERSIZE - 1);
+                    readValue = read(sockFd, buffer, BUFFERSIZE - 1);
 
-    if (readValue < 0) {
-        printf("Reading failed: Error message: %s\n", strerror(errno));
-    } else {
-        // printf("%s\n", buffer);
-    }
+                    if (readValue < 0) {
+                        printf("Reading failed: Error message: %s\n", strerror(errno));
+                    }
 
-    printf("Closing socket\n");
+                    printf("Closing socket\n");
 
-    token = strstr(buffer, lineBeforeBody);
+                    token = strstr(buffer, lineBeforeBody);
 
-    if (token != NULL) {
-        token += strlen(lineBeforeBody);
-    }
+                    if (token != NULL) {
+                        token += strlen(lineBeforeBody);
+                    }
 
-    FILE *newFile = fopen(filename, "w+");
+                    FILE *newFile = fopen(filename, "w+");
 
-    if (newFile == NULL) {
-        printf("Failed to open file - Error message: %s\n", strerror(errno));
-        return 1;
-    }
+                    if (newFile == NULL) {
+                        printf("Failed to open file - Error message: %s\n", strerror(errno));
+                        return 1;
+                    }
 
-    fputs(token, newFile);
+                    fputs(token, newFile);
 
-    fclose(newFile);
+                    fclose(newFile);
 
-    newFile = fopen(filename, "r");
+                    newFile = fopen(filename, "r");
 
-    fseek(newFile, 0, SEEK_END);
-    int position = ftell(newFile);
+                    fseek(newFile, 0, SEEK_END);
+                    int position = ftell(newFile);
 
-    printf("Position: %d\n", position);
+                    printf("Position: %d\n", position);
 
-    fseek(newFile, 0, SEEK_SET);
+                    fseek(newFile, 0, SEEK_SET);
 
-    for (int j = 0; j < position; ++j) {
-        char temp = getc(newFile);
-        if(temp == EOF){
-            break;
+                    for (int j = 0; j < position; ++j) {
+                        char temp = getc(newFile);
+                        if (temp == EOF) {
+                            break;
+                        }
+                        readFromFile[j] = temp;
+                    }
+
+                    printf("%s\n", readFromFile);
+
+                    fclose(newFile);
+
+                    close(sockFd);
+                    sockFd = -1;
+                }
+            }
         }
-        readFromFile[j] = temp;
     }
-
-    printf("1\n");
-    printf("%s\n", readFromFile);
-    printf("2\n");
-
-    fclose(newFile);
-
-    close(sockFd);
-    sockFd = -1;
-
     return 0;
 }
