6c7b85946506a924faa8f885784da3c3cc4f3566
stefannaeve <stefan.naeve@hotmail.com>
2024-04-21 19:35:05 +0100

    Finished binary task

--- ExamPrep/Leksjon8/task2/src/main.c
+++ ExamPrep/Leksjon8/task2/src/main.c
@@ -4,15 +4,57 @@
 #define BIGGEST_BIT_SIZE 64
 #define SMALLEST_BIT_SIZE 4
 
-int printBinaryFromInteger(int64_t integer, int bitSize);
+int convertToBinary(int8_t *binary, int64_t integer, int bitSize);
+int printBinary(int8_t *binary, int bitSize);
+int invertBinary(int8_t *binary, int bitSize);
+int binaryToChar(int8_t *binary, int *newNumber);
+
+// Unsigned
+// 1-bit max == 2/1
+// 4-bit max == 16/15
+// 8-bit max == 256/255 (Size of unsigned char or int8_t)
+// 16-bit max == 65536/65535
+// 32-bit max == 4294967296/4294967295 (Size of a regular int)
+// 64-bit max == A fucking lot
 
 int main(){
-    printBinaryFromInteger(328, 16);
+    int bitSize = 8;
+    int8_t binary[bitSize];
+    int number = 'a';
+    int newNumber;
+
+    printf("%c\n", number);
+
+    convertToBinary(binary, number, bitSize);
+    printBinary(binary, bitSize);
+
+    binaryToChar(binary, &newNumber);
+
+    printf("%c\n", newNumber);
     return 0;
 }
 
-int printBinaryFromInteger(int64_t integer, int bitSize){
-    char binary[bitSize+1];
+int binaryToChar(int8_t *binary, int *newNumber){
+    int asciiBitSize = 8;
+    int j = 0;
+
+    for (int i = asciiBitSize-1; i >= 0; i--) {
+        char mask = binary[j++] << i;
+        (*newNumber) += mask;
+        printf("mask: %d\n", mask);
+
+    }
+
+    return 0;
+}
+
+int invertBinary(int8_t *binary, int bitSize){
+    for (int i = 0; i < bitSize; ++i) {
+        binary[i] = !binary[i];
+    }
+}
+
+int convertToBinary(int8_t *binary, int64_t integer, int bitSize){
 
     if (bitSize > BIGGEST_BIT_SIZE){
         printf("Invalid input: bit size exceeds 64 bits");
@@ -25,16 +67,19 @@ int printBinaryFromInteger(int64_t integer, int bitSize){
     }
 
     for (int i = bitSize-1; i >= 0; i--) {
-        binary[i] = (integer & 1) ? '1' : '0';
+        binary[i] = (integer & 1) ? 1 : 0;
         integer >>= 1;
     }
-    binary[bitSize] = '\0';
-    printf("\n");
 
+    return 0;
+}
+
+int printBinary(int8_t *binary, int bitSize){
     for (int i = 0; i < bitSize; ++i) {
         if ((i%8) == 0 && i != 0){
             printf(" ");
         }
-        printf("%c", binary[i]);
+        printf("%d", binary[i]);
     }
+    printf("\n");
 }
